a9b3cb6d1be46353f5d70d57e1522207555adade
Iliad Hemmatjoo <124857326+Iliaddh@users.noreply.github.com>
2023-12-16 22:20:00 -0500

    Update Assignment3_40205441.java

--- Assignment3_40205441.java
+++ Assignment3_40205441.java
@@ -1,16 +1,4 @@
-/**
- * Full Name: Ilyad Hemmatjoo
- * ID: 40205441
- * Course: COMP249
- * Assignment: Assignment 1
- * Due Date: November 11, 2023
- */
 
-// -----------------------------------------------------
-// Assignment (2)
-// Class: (1)
-// Written by: (Ilyad Hemmatjoo ---- ID: 40205441)
-// -----------------------------------------------------
 
 //This program Opens a file that includes some .txt file containing some information about books. First, in check whether it's date and price and ISBN format is syntatically correct, then it stores
 //the valid books into genre specific .txt files. If a syntax error found , it will record it in a text file. In the second part of the program, it reads those syntatically valid books, and this time checking for semantic errors.
